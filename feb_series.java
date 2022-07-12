0c73f82e2521f4bf1719bc213e80099e67ad744a
sai kiran madala <85841113+sai-kiran-99@users.noreply.github.com>
2022-07-13 00:00:11 +0530

    feb_series

---
+++ feb_series.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class feb_series{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int length;
+        System.out.println("Enter the length:");
+        length=sc.nextInt();
+        System.out.println(length);
+        int[] num=new int[length];
+        num[0]=0;
+        num[1]=1;
+        System.out.println(num[0] + " " + num[1]);
+        for(int i=2;i<length;i++){
+            num[i]=num[i-2]+num[i-1];
+            System.out.println(num[i] + "");
+        }
+
+
+    }
+}
