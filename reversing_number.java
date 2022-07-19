85b351a226b2bdba5d7e78910d2d7fd38a1fe6d7
sai kiran madala <85841113+sai-kiran-99@users.noreply.github.com>
2022-07-19 23:12:40 +0530

    Reversing a number

--- reversing_number.java
+++ reversing_number.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class reversing_number {
-    
+    public static void main(String[] args){
+        int remainder,reverse;
+        Scanner num=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int number=num.nextInt();
+        while(number!=0){
+            remainder=number%10;//lastdigit
+            reverse=0*remainder+10;
+            number=number/10;
+        }
+        System.out.println
+    }
 }
