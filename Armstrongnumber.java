86be6802b8f5f26105cac62aa9cf6635547f388c
sai kiran madala <85841113+sai-kiran-99@users.noreply.github.com>
2022-07-19 22:56:44 +0530

    Print Ascii value

--- Armstrongnumber.java
+++ Armstrongnumber.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
+//An Armstrong number is a positive m-digit number that is equal to the sum of the mth powers of their digits
+//ex:1=1^1=1,22=2^2+2^2=8 not an armstrong,153: 1^3 + 5^3 + 3^3 = 1 + 125+ 27 = 153
+//note:There is no two digit armstrong number
 public class Armstrongnumber {
     public static void main(String[] args){
-        
+       Scanner arm=new Scanner(System.in);
+       int n;
+       System.out.println("Enter the value of n:");
+       n=arm.nextInt();
+       System.out.println(n);
+
+
 
     }
     
---
+++ Ascii.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ascii{
+    public static void main(String[] args){
+          Scanner character=new Scanner(System.in);
+          System.out.println("enter the character:");
+          char ch=character.next().charAt(0);
+         int AScci=ch;
+         System.out.println(AScci);
+
+    }
+}
